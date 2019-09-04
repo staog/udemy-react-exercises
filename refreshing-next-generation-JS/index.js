2a356e2d4c95b149c4e56c94319544f937042706
Milan V. Kecojević <staog@gmx.com>
2019-09-04 18:54:46 +0200

    Update index.js

--- refreshing-next-generation-JS/index.js
+++ refreshing-next-generation-JS/index.js
@@ -60,7 +60,7 @@ import {smth as Smith} from './jsfile'
 import * as someObjectName from './jsfile'
 
 
-// classes in JS that can inherit from another classes using keyword extends, and then, if we use constructor in our new Class (but we don't have to), than we must use 'super' method inside of constructor to create all thing that parent has
+// classes in JS that can inherit from another classes using keyword extends, and then, if we use constructor in our new Class (but we don't have to), than we must use 'super' method inside of constructor to create all things that parent Class has
 
 class Human {
   constructor() {
@@ -111,4 +111,90 @@ myMethod() {...}
 myMehtod = () => {...}
 
 
-// spread and rest operators
+// spread and rest operators (actually it's only one operator ... , but how we'll call it depends on where we use it)
+
+// spread operator is used to spread array elements or object properties
+
+const newArray = [...oldArray, 1, 2]
+
+const newObject = {...oldObject, newProp: 5}
+
+// array example:
+
+const arr = [1, 2, 3];
+
+const newArr = [...arr, 4];
+
+console.log(newArr); // it outputs this [1, 2, 3, 4]
+
+// object example
+
+const person = {
+  name: "Milan"
+}
+
+const newPerson = {
+  ...person,
+  age: 38
+}
+
+console.log(newPerson); // it will output this
+
+[object Object] {
+  age: 38,
+  name: "Milan"
+}
+
+// rest operator is used to merge a list of function arguments into an array so that we can than use array methods
+
+function sortArgs(...args) {
+  return args.sort();
+}
+
+// function example
+
+const filter = (...args) => {
+  return args.filter(el => el === 1);
+}
+
+console.log(filter(1, 2, 3)); // returns [1]
+
+
+// destructuring allows us to extract array elements or object properties and store them in variables
+
+[a, b] = ["Hello", "Milan"]
+
+console.log(a) // returns Hello
+console.log(b) // returns Milan
+
+{name} = {name: "Milan", age: 38}
+
+console.log(name) // returns Milan
+console.log(age) // returns undefined
+
+
+const numbers = [1, 2, 3];
+
+[num1, num2] = numbers;
+
+console.log(num1, num2);
+
+
+// reference and primitive types
+
+const number = 1;
+const num2 = number; // creates a copy of number (this applies to all primitive types)
+
+const person = {
+  name: "Milan"
+}
+
+const secondPerson = person;
+
+console.log(secondPerson); // this is reference type (JS engine just copies the pointer, doesn't make a new copy)
+
+// if we want to make a real copy, we can use spread operater
+
+const secondPerson = {
+  ...person
+}
