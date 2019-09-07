adbd39d57e1224f42ff44cfc31a5779473e028c2
Milan V. Kecojević <staog@gmx.com>
2019-09-07 20:03:04 +0200

    Update App.js

--- understanding-the-base-features-and-syntax/App.js
+++ understanding-the-base-features-and-syntax/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm React App!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm React App! :-)</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This is what jsx gets compiled to'));
+  }
 }
 
 export default App;
